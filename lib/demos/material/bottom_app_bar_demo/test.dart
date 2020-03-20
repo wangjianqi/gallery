e3df8103980badfe36abe02791f0f5567157ad74
Jose Alba Aguado <jaguado@google.com>
2020-03-20 12:39:08 +0100

    Moved material components into seperate folders and added bottom app bar demo

---
+++ lib/demos/material/bottom_app_bar_demo/test.dart
@@ -0,0 +1,10 @@
+import 'package:gallery/demos/material/bottom_app_bar_demo/main.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: BottomAppBarDemo(),
+    ),
+  );
+}
